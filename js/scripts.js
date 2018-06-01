ba961301a3fd8671a7a40260be5b4a0bce50528c
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-01 10:40:32 -0700

    update scripts to calculate questions 5-10 as part of results equation

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function(event) {
   $("form#quizquestions").submit(function(event) {
-    var points = parseInt($("input:radio[name=question1]:checked").val()) + parseInt($("input:radio[name=question2]:checked").val()) + parseInt($("input:radio[name=question3]:checked").val()) + parseInt($("input:radio[name=question4]:checked").val()) + parseInt($("input:radio[name=question5]:checked").val())
+    var points = parseInt($("input:radio[name=question1]:checked").val()) + parseInt($("input:radio[name=question2]:checked").val()) + parseInt($("input:radio[name=question3]:checked").val()) + parseInt($("input:radio[name=question4]:checked").val()) + parseInt($("input:radio[name=question5]:checked").val()) +
+    parseInt($("input:radio[name=question6]:checked").val()) +
+    parseInt($("input:radio[name=question7]:checked").val()) +
+    parseInt($("input:radio[name=question8]:checked").val()) +
+    parseInt($("input:radio[name=question9]:checked").val()) +
+    parseInt($("input:radio[name=question10]:checked").val()) 
 
     if (points >= 41) {
       $('.result').html('<h2 class="center">React and C#!</h2><img class="center" src="img/reactC.png"/>');
