b3e640b8fe337825363a482a7d5fa6987762c3c5
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-01 10:05:30 -0700

    add two additional questions and change ranging values to account for 10 questions

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ $(document).ready(function(event) {
   $("form#quizquestions").submit(function(event) {
     var points = parseInt($("input:radio[name=question1]:checked").val()) + parseInt($("input:radio[name=question2]:checked").val()) + parseInt($("input:radio[name=question3]:checked").val()) + parseInt($("input:radio[name=question4]:checked").val()) + parseInt($("input:radio[name=question5]:checked").val())
 
-    if (points >= 25) {
+    if (points >= 41) {
       $('.result').html('<h2 class="center">React and C#!</h2><img class="center" src="img/reactC.png"/>');
-    } else if (points >= 20 ) {
+    } else if (points >= 21 ) {
       $('.result').html('<h2 class="center">Ruby on Rails!</h2><img class="center" src="img/ruby.png"/>');
-    } else if (points >= 15) {
+    } else if (points >= 1) {
       $('.result').html('<h2 class="center">Design/CSS!</h2><img class="center" src="img/design.png"/>');
     } else {
       $('.result').html('<h2 class="center">One or more questions left blank.</h2><img class="center" src="img/error.png"/>');
