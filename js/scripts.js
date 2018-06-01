440b98a4b7b4b92f07a269c856b5233e175c0641
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-01 10:54:33 -0700

    change scripts algorithm to make react/C# the media balance between front and back end

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,9 @@ $(document).ready(function(event) {
 
     if (points >= 41) {
       $('.result').html('<h2 class="center">Ruby on Rails!</h2><img class="center" src="img/ruby.png"/>');
-    } else if (points >= 21 ) {
+    } else if (points >= 30 ) {
       $('.result').html('<h2 class="center">React and C#!</h2><img class="center" src="img/reactC.png"/>');
-    } else if (points >= 1) {
+    } else if (points >= 20) {
       $('.result').html('<h2 class="center">Design/CSS!</h2><img class="center" src="img/design.png"/>');
     } else {
       $('.result').html('<h2 class="center">One or more questions left blank.</h2><img class="center" src="img/error.png"/>');
