3f5c21bcb64255e0e37450764ef82f5a88bf2a1e
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-01 15:55:39 -0700

    reformat scripts.js and styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 (function() {
   'use strict';
   window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
     var validation = Array.prototype.filter.call(forms, function(form) {
       form.addEventListener('submit', function(event) {
         if (form.checkValidity() === false) {
           event.preventDefault();
           event.stopPropagation();
-          $('html, body').animate({ scrollTop: 0 }, 'medium');
+          $('html, body').animate({
+            scrollTop: 0
+          }, 'medium');
         }
         form.classList.add('was-validated');
       }, false);
@@ -24,15 +24,15 @@ $(document).ready(function(event) {
     $("#greeting").html(firstName + " " + lastName);
 
     var points = parseInt($("input:radio[name=question1]:checked").val()) + parseInt($("input:radio[name=question2]:checked").val()) + parseInt($("input:radio[name=question3]:checked").val()) + parseInt($("input:radio[name=question4]:checked").val()) + parseInt($("input:radio[name=question5]:checked").val()) +
-    parseInt($("input:radio[name=question6]:checked").val()) +
-    parseInt($("input:radio[name=question7]:checked").val()) +
-    parseInt($("input:radio[name=question8]:checked").val()) +
-    parseInt($("input:radio[name=question9]:checked").val()) +
-    parseInt($("input:radio[name=question10]:checked").val())
+      parseInt($("input:radio[name=question6]:checked").val()) +
+      parseInt($("input:radio[name=question7]:checked").val()) +
+      parseInt($("input:radio[name=question8]:checked").val()) +
+      parseInt($("input:radio[name=question9]:checked").val()) +
+      parseInt($("input:radio[name=question10]:checked").val())
 
     if (points >= 41) {
       $('.result').html('<h2 class="center">You got...<br>Ruby on Rails!</h2><img class="center" src="img/ruby.png"/>');
-    } else if (points >= 30 ) {
+    } else if (points >= 30) {
       $('.result').html('<h2 class="center">You got...<br>React and C#!</h2><img class="center" src="img/reactC.png"/>');
     } else if (points >= 20) {
       $('.result').html('<h2 class="center">You got...<br>Design/CSS!</h2><img class="center" src="img/design.png"/>');
