983af601f8145324c5c4d85ff451bfcb813b199e
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-01 09:10:02 -0700

    begin adding scripts.js to calculate form values and return a result

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,11 @@ $(document).ready(function(event) {
     var points = parseInt($("input:radio[name=question1]:checked").val()) + parseInt($("input:radio[name=question2]:checked").val()) + parseInt($("input:radio[name=question3]:checked").val()) + parseInt($("input:radio[name=question4]:checked").val()) + parseInt($("input:radio[name=question5]:checked").val())
 
     if (points >= 25) {
-      $('.result').html('<h2 class="center">Margot Tenenbaum</h2><img class="center" src="img/margot.jpg"/>');
+      $('.result').html('<h2 class="center">React and C#!</h2><img class="center" src="img/reactC.png"/>');
     } else if (points >= 20 ) {
-      $('.result').html('<h2 class="center">Klaus Daimler</h2><img class="center" src="img/klaus.jpeg"/>');
+      $('.result').html('<h2 class="center">Ruby on Rails!</h2><img class="center" src="img/ruby.png"/>');
     } else if (points >= 15) {
-      $('.result').html('<h2 class="center">Max Fischer</h2><img class="center" src="img/max.jpg"/>');
-    } else if (points >= 10) {
-      $('.result').html('<h2 class="center">Steve Zissou</h2><img class="center" src="img/steve.jpg"/>');
-    }  else if (points >= 5) {
-      $('.result').html('<h2 class="center">Dignan</h2><img class="center" src="img/dignan.jpg"/>');
-    } else if (points === 5) {
-      $('.result').html('<h2 class="center">Mr. Fox</h2><img class="center" src="img/fox.jpg"/>');
+      $('.result').html('<h2 class="center">Design/CSS!</h2><img class="center" src="img/design.png"/>');
     } else {
-      $('.result').html('<h2 class="center">Error: One or more questions left blank.</h2>');;
+      $('.result').html('<h2 class="center">One or more questions left blank.</h2><img class="center" src="img/error.png"/>');;
     }
