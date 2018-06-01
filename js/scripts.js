8c9cc923875e5b5cc33c50033b1f69f75b93c397
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-01 10:43:02 -0700

    change value of react from 6 to 4, change value of ruby from 4 to 6

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ $(document).ready(function(event) {
     parseInt($("input:radio[name=question7]:checked").val()) +
     parseInt($("input:radio[name=question8]:checked").val()) +
     parseInt($("input:radio[name=question9]:checked").val()) +
-    parseInt($("input:radio[name=question10]:checked").val()) 
+    parseInt($("input:radio[name=question10]:checked").val())
 
     if (points >= 41) {
-      $('.result').html('<h2 class="center">React and C#!</h2><img class="center" src="img/reactC.png"/>');
-    } else if (points >= 21 ) {
       $('.result').html('<h2 class="center">Ruby on Rails!</h2><img class="center" src="img/ruby.png"/>');
+    } else if (points >= 21 ) {
+      $('.result').html('<h2 class="center">React and C#!</h2><img class="center" src="img/reactC.png"/>');
     } else if (points >= 1) {
       $('.result').html('<h2 class="center">Design/CSS!</h2><img class="center" src="img/design.png"/>');
     } else {
